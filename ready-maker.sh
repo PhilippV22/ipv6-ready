16ce31b9fc3a3333b2b80436896274e83515d553
PhilippV22 <88344217+PhilippV22@users.noreply.github.com>
2022-11-25 21:09:31 +0100

    1.0.2

--- ready-maker.sh
+++ ready-maker.sh
@@ -1,13 +1,14 @@
-echo "Getting Systeminfo...";
-ipv6=$(ip -6 addr show dev eth0 scope global | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d');
-echo "$ipv6";
+echo "Getting Systeminfo..."
+ipv6=$(ip -6 addr show dev eth0 scope global | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d')
+echo "$ipv6"
 clear
 
-echo "Do you want to setup NO-IP-DUC? (Y/n): $noipyn";
+echo "Do you want to setup NO-IP-DUC? (Y/n)"
+read noipyn
 clear
-echo "Do you want to setup Apache-DUC? (Y/n): $apacheyn";
-clear
-echo "Setup can beginn";
+echo "Do you want to setup Apache-DUC? (Y/n)"
+read apacheyn
+echo "Setup can beginn"
 
 if [ $noipyn == 'Y' ] || [ $noipyn == 'y' ]
 then
@@ -16,11 +17,14 @@ then
     cd /root/
     wget https://nextcloud.longrise.biz/download/noipduc.service
     clear
-    echo "What is your E-Mail from NO-IP?: $noipemail";
+    echo "What is your E-Mail from NO-IP?"
+    read noip-email
     clear
-    echo "What is your Password from NO-IP?: $noippassword";
+    echo "What is your Password from NO-IP?"
+    read noip-password
     clear
-    echo "What is the Domain that you want to update?: $noipdomain";
+    echo "What is the Domain that you want to update?"
+    echo noip-domain
     clear
     printf "account = ('$noipemail', '$noippassword')\ntoBeUpdated = '$noipdomain'\nwhichOne = 1\nupdateFrequency = 10\nimport requests\nimport subprocess\nfrom time import sleep\nimport os\nupdateurl = "http://dynupdate.no-ip.com/nic/update"\nlastaddr = str() \nwhile True:\n    hostnameReturn = subprocess.Popen("hostname -I", shell=True, stdout=subprocess.PIPE)\n    ipv6addrs = hostnameReturn.stdout.read()\n	   ipv6addr = str(ipv6addrs).split(' ')[whichOne]\n	   print(ipv6addr)  # print out just to be sure\n    parameters = {'hostname': toBeUpdated, 'myipv6': ipv6addr}\n    requestHeader = {'User-Agent': 'Personal noipv6-duc_1_0.py/linux-v5.0'}\n    if lastaddr != ipv6addr:\n        lastaddr = ipv6addr # update the latest ip address\n         serverreturn = requests.get(url = updateurl, params = parameters, auth=account)\n         if(serverreturn.status_code == 911):\n            print("Updating paused. No-IP server asks to stop requesting due to a server-side error for 30 minutes.")\n            sleep(1800) # sleep 1800 seconds = 30 minutes\n        print(serverreturn.url)\n         print(serverreturn.text)\n        os.system('/root/duc.sh')\n         print("updated. noice job!")\n    sleep(updateFrequency)" > /root/noipduc.py
     printf "[Unit]\nDescription=NOIPDUC\n\n[Service]\nType=simple\nExecStart=/bin/python3 /root/noipduc.py\nRestart=always\nRestartPreventExitStatus=yes\n\n[Install]\nWantedBy=multi-user.target" > /lib/systemd/system/noipduc.service
@@ -31,10 +35,10 @@ then
     printf "3 * * * * /bin/bash /root/duc.sh && /bin/systemctl restart noipduc\n@reboot /bin/bash /root/duc.sh && /bin/systemctl restart noipduc" >> mycron
     crontab mycron
     rm mycron
-    echo "NOIP-DUC is installd!!!";
+    echo "NOIP-DUC is installd!!!"
 else
     apt update && apt upgrade -y
-    echo "NOIP-DUC dont get installd";
+    echo "NOIP-DUC dont get installd"
 fi
 
 clear
@@ -46,7 +50,7 @@ then
     wget https://nextcloud.longrise.biz/download/duc.sh
     chmod 777 duc.sh
 else
-    echo "Apache dont get installed";
+    echo "Apache dont get installed"
 fi
 
  
