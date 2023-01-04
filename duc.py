2f3e9e3d3a1b3c8952f479d9246395f6d2f874fd
PhilippV <88344217+PhilippV22@users.noreply.github.com>
2023-01-04 17:52:44 +0100

    Create duc.py

---
+++ duc.py
@@ -0,0 +1,20 @@
+import requests
+
+# NO-IP Benutzername und Passwort
+username = "reinholzphilipp463@gmail.com"
+password = "77Philipp335698"
+
+# Hostname, den Sie bei NO-IP registriert haben
+hostname = "shop.longrise.biz"
+
+# Die aktuelle globale IPv6-Adresse abrufen
+ipv6_address = requests.get("http://ipv6.icanhazip.com").text.strip()
+
+# Die IPv6-Adresse bei NO-IP aktualisieren
+response = requests.get(f"http://dynupdate.no-ip.com/nic/update?hostname={hostname}&myip={ipv6_address}", auth=(username, password))
+
+# Überprüfen, ob die Aktualisierung erfolgreich war
+if "good" in response.text.lower():
+  print("Die IPv6-Adresse wurde erfolgreich aktualisiert.")
+else:
+  print("Fehler beim Aktualisieren der IPv6-Adresse.")
